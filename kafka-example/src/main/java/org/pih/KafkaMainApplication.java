2deee49c4be40ae15adab25ada2fd59bd2af6926
girish2909 <girish.mishra1@gmail.com>
2022-12-12 18:40:22 +0530

    Commited unnecessary commandRunnder calling for KafkaTemplate

--- kafka-example/src/main/java/org/pih/KafkaMainApplication.java
+++ kafka-example/src/main/java/org/pih/KafkaMainApplication.java
@@ -1,11 +1,8 @@
 package org.pih;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaMainApplication{
@@ -17,11 +14,11 @@ public class KafkaMainApplication{
         SpringApplication.run(KafkaMainApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
-        return args -> {
-            for (int i=0; i<100;i++)
-            kafkaTemplate.send(topicName, "hello Kafka : "+i);
-        };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+//        return args -> {
+//            for (int i=0; i<100;i++)
+//            kafkaTemplate.send(topicName, "hello Kafka : "+i);
+//        };
+//    }
 }
