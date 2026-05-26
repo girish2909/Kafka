1b1e2578ae1ed443e48b637c8b32a6fb75d59899
gkumarm-oss <gkumarm@liverpool.com.mx>
2026-05-26 09:52:41 +0530

    Dead Letter Queue better implementation

---
+++ kafka-example/src/main/java/org/pih/repository/FailedMessageRepository.java
@@ -0,0 +1,9 @@
+package org.pih.repository;
+
+import org.pih.entity.FailedMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FailedMessageRepository extends JpaRepository<FailedMessage, Long> {
+}
